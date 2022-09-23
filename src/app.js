7c5c75e76372ae28a7eecac7b14bbdec6e3d6b04
Hawru <imharu01@gmail.com>
2022-09-23 17:53:31 -0500

    cambios del puerto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 //Carpeta Public
 app.use(express.static('./public'));
@@ -10,7 +11,7 @@ app.set('views',  path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 //Port
-app.listen(3004, () => console.log('server corriendo en puerto 3004'));
+app.listen(PORT, () => console.log('server corriendo en puerto 3004'));
 
 //Routes
 app.use('/', require('./routes/index.routes'));
