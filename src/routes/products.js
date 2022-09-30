b40f6b328af03031059682db38cbe34f3f2dbff5
santiagom86 <santiagomartinez86@gmail.com>
2022-09-30 17:38:06 -0300

    instalo multer y lo configuro en el enrutador y los forms

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
+const multer = require('multer');
+
+//Configuración Multer
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+       cb(null, path.join(__dirname,'../../public/images/products'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName = Date.now() + path.extname(file.originalname);   // milisegundos y extensión de archivo original
+       cb(null, imageName);         
+    }
+});
+
+const uploadFile = multer({ storage: multerDiskStorage });
 
 //routes
 router.get('/list', productsController.listaProducto)
 router.get('/:id', productsController.verProducto);
 router.get('/create', productsController.crearProducto)
-router.post('/create', productsController.almacenarProducto)
+router.post('/create', uploadFile.single('primary_image'), productsController.almacenarProducto)
 router.get('/:id/update', productsController.actualizarProducto)
-router.put('/:id/update', productsController.guardarProducto)
+router.put('/:id/update', uploadFile.single('primary_image'), productsController.guardarProducto)
 
 module.exports = router;
