b931c77035425168af37b5358186bd4604a6ae1d
santiagom86 <santiagomartinez86@gmail.com>
2022-11-05 15:03:58 -0300

    prueba fallida validacion y creacion de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const fs = require('fs');
+const usersFilePath = path.join(__dirname, '../database/data/usuarios.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const { validationResult } = require('express-validator');
 
 const usersController = {
     register: (req, res) => {
         res.render('users/register');
     },
     userRegister: (req, res) => {
-        //Funcion para registrar el usuario
+        let errors = validationResult(req);
+        if(errors.isEmpty()){
+            let userData = req.body;
+            let idNewUser = (users[users.length-1].id)+1;
+            let profile_image = "users/" + req.file.filename;
+    
+            let newUser ={
+                "id": idNewUser,
+                "username": userData.username,
+                "email": userData.email,
+                "password": userData.password,
+                "profile_image": profile_image
+            };
+    
+            users.push(newUser);
+            fs.writeFileSync(juegosFilePath,JSON.stringify(users, null, " "),'utf-8');
+            res.redirect('/');
+        } else {
+            res.render('users/register', {errors: errors.mapped(), old:req.body});
+        }
     },
     login: (req, res) => {
         res.render('users/login');
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 const usersController = require('../controllers/usersController');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { body }  = require('express-validator');
 
 //Configuración Multer
 const storageProfileImage = multer.diskStorage({
@@ -16,9 +18,16 @@ const storageProfileImage = multer.diskStorage({
 
 const uploadProfileImage = multer({ storage: storageProfileImage });
 
+//Validacion Formulares
+let validacionRegistro = [
+    body('username').notEmpty().withMessage('Campo obligatorio'),
+    body('email').notEmpty().withMessage('Campo obligatorio').isEmail().withMessage('Ingresa un correo válido'),
+    body('password').notEmpty().withMessage('Campo obligatorio').isLength({min:5, max:10}).withMessage('La contraseña debe contener entre 5 y 10 caractéres.'),
+]
+
 //routes
 router.get('/register', usersController.register);
-router.post('/register',  uploadProfileImage.single('profile_image'), usersController.userRegister);
+router.post('/register',  validacionRegistro, uploadProfileImage.single('profile_image'), usersController.userRegister);
 router.get('/login', usersController.login);
 router.post('/login', usersController.userLogin);
 
