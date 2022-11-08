b41da6d7c5646905ae7a97e73774795b70a05277
Hawru <imharu01@gmail.com>
2022-11-07 20:46:00 -0500

    cambio res render authlogmiddleware

--- src/middleWares/authLogMiddleWare.js
+++ src/middleWares/authLogMiddleWare.js
@@ -1,6 +1,6 @@
 function authLogMiddleWare(req, res, next){
     if(!req.session.user){
-        return res.render('404', {auth: 'No puedes entrar a esta parte'})
+        return res.render('login', {auth: 'No puedes entrar a esta parte'})
     } 
     next();
 }
