470685544f13fe8c7a3da28abb90031a0437f3a6
Hawru <imharu01@gmail.com>
2022-11-21 19:11:24 -0500

    modelo producto v2

---
+++ src/database/models1/seeders.sql
@@ -0,0 +1,15 @@
+INSERT INTO `equipo7_store`.`plataforms` (`name`, `title`, `icon`) VALUES ('xbox', 'XBOX', 'fa-brands fa-xbox');
+INSERT INTO `equipo7_store`.`plataforms` (`name`, `title`, `icon`) VALUES ('playstation', 'Play Station', 'fa-brands fa-playstation');
+INSERT INTO `equipo7_store`.`plataforms` (`name`, `title`, `icon`) VALUES ('pc', 'PC', 'fa-brands fa-windows');
+INSERT INTO `equipo7_store`.`plataforms` (`name`, `title`, `icon`) VALUES ('mac', 'MAC', 'fa-brands fa-apple');
+
+INSERT INTO `equipo7_store`.`score_types` (`name`, `title`) VALUES ('difficulty', 'Dificultad');
+INSERT INTO `equipo7_store`.`score_types` (`name`, `title`) VALUES ('graphics', 'Graficos');
+
+INSERT INTO `equipo7_store`.`requirement_types` (`name`, `title`) VALUES ('video', 'Placa de video');
+INSERT INTO `equipo7_store`.`requirement_types` (`name`, `title`) VALUES ('cpu', 'Procesador');
+INSERT INTO `equipo7_store`.`requirement_types` (`name`, `title`) VALUES ('memory', 'Memoria');
+INSERT INTO `equipo7_store`.`requirement_types` (`name`, `title`) VALUES ('disk', 'Almacenamiento');
+
+INSERT INTO `equipo7_store`.`version_types` (`name`, `title`) VALUES ('standard', 'Standard');
+INSERT INTO `equipo7_store`.`version_types` (`name`, `title`) VALUES ('deluxe', 'Deluxe');
